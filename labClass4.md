56ecd12dfb265e15caf3c8491580f89bcae838ad
Jonathon Stillson <jcstillson77@gmail.com>
2023-07-02 23:53:48 -0400

    finished lab 4

---
+++ labClass4.md
@@ -0,0 +1,14 @@
+# Lab 4 Notes
+
+## Forms and Filter
+
+### Feature 1 Filter by Number of Horns
+
+Given that a user is presented with filtering options when the user clicks on one option then the images should be filtered accordingly.
+
+Using the `Form` component of `react-bootstrap` build a drop down menu so that the user can choose to filter by number of horns.
+When the user chooses one of the options, the correct images should be displayed.
+
+Time estimate: 80 mins
+Time Start: 2215
+Time Finish: 
--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-import data from './data/data.json';
 import SelectedBeast from './SelectedBeast';
-// import './css/App.css';
+import data from './data/data.json';
+import './css/App.css';
+import Form from 'react-bootstrap/Form';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,9 +15,10 @@ class App extends React.Component {
       beastTitle: '',
       beastImg: '',
       beastDescription: '',
-    }
+      horns: '',
+      sortedData: data,
+    };
   }
-
 
   handleOpenModal = (title, image_url, description) => {
     this.setState({
@@ -24,26 +26,70 @@ class App extends React.Component {
       beastTitle: title,
       beastImg: image_url,
       beastDescription: description,
-    })
-  }
+    });
+  };
 
   handleCloseModal = () => {
     this.setState({
       showModal: false,
-    })
-  }
+    });
+  };
+
+  handleSelect = (event) => {
+    let selected = parseInt(event.target.value);
+
+    if (selected === 1) {
+      let newData = data.filter((element) => element.horns === 1);
+      this.setState({
+        sortedData: newData,
+      });
+    } else if (selected === 2) {
+      let newData = data.filter((element) => element.horns === 2);
+      this.setState({
+        sortedData: newData,
+      });
+    } else if (selected === 3) {
+      let newData = data.filter((element) => element.horns === 3);
+      this.setState({
+        sortedData: newData,
+      });
+    } else if (selected === 100) {
+      let newData = data.filter((element) => element.horns === 100);
+      this.setState({
+        sortedData: newData,
+      });
+    } else if (selected === 0) {
+      this.setState({
+        sortedData: data,
+      });
+    }
+  };
 
   render() {
     return (
       <>
         <Header />
-        <Main data={data} handleOpenModal={this.handleOpenModal} />
-        <SelectedBeast handleCloseModal={this.handleCloseModal} showModal={this.state.showModal} beastTitle={this.state.beastTitle}
-          beastImg={this.state.beastImg} beastDescription={this.state.beastDescription} beastObj={data}>
-        </SelectedBeast>
+        <Form.Select onChange={this.handleSelect} aria-label="hornedForm">
+          <option>Filter beasts by horns 🎺</option>
+          <option value="0">All</option>
+          <option value="1">1 horn</option>
+          <option value="2">2 horns</option>
+          <option value="3">3 horns</option>
+          <option value="100">100 horns</option>
+        </Form.Select>
+        <Main data={this.state.sortedData} handleOpenModal={this.handleOpenModal} />
+        <SelectedBeast
+          handleCloseModal={this.handleCloseModal}
+          showModal={this.state.showModal}
+          beastTitle={this.state.beastTitle}
+          beastImg={this.state.beastImg}
+          beastDescription={this.state.beastDescription}
+          beastObj={data}
+        ></SelectedBeast>
         <Footer />
       </>
-    )
+    );
   }
 }
+
 export default App;
